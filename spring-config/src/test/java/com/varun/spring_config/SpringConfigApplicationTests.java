eacd58916224e623525790b31c19b5cb9ee796a5
blogwithvarun <varunsha1903@gmail.com>
2024-06-16 23:24:11 +0530

    initial changes

---
+++ spring-config/src/test/java/com/varun/spring_config/SpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.spring_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
